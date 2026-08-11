87093a3782ac424585c56b7c3453e204d55b645b
sachin choudhary <amisachinchoudhary@gmail.com>
2026-08-11 10:23:27 +0530

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool fun(int speed,int time,vector<int>& piles){
+        int count=0;
+        for(int it:piles){
+            count += it/speed;
+            count += (it%speed==0)?0:1;
+            
+        }
+        return count <= time;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size();
+        if(h < n) return -1;
+        int max_value = INT_MIN;
+        for(int it:piles){
+            max_value = max(max_value,it);
+        } 
+        int st = 1,end=max_value;
+        int ans=0 ;
+        while(st < end){
+            int mid = st + (end-st)/2;
+            if(fun(mid,h,piles)){
+                end = mid;
+                ans = mid;
+            } 
+            else{
+                st = mid+1;
+            }
+        }
+        return st;
+    }
+};
